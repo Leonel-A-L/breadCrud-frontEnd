9e32d74103f03f841757eea06b11baae03661cc9
Leonel-A-L <lamelas.leonel@gmail.com>
2023-06-13 21:24:45 -0400

    added a navbar and a post(New) to connect between the back and front

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Bread from "./components/Bread";
+import New from "./components/New"
 
 function App() {
   return (
     <div>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/bread/:id" element={<Bread />} />
+          <Route path="/new" element={<New />} />
         </Routes>
       </Router>
     </div>
